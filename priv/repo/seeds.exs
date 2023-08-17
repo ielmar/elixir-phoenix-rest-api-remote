5ea821498305f33289da72fcb62f6b3656071086
Elmar Ismayilov <bamazy@gmail.com>
2023-08-17 09:16:49 +0800

    seed file to import data added

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,31 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias BeExercise.{Repo, Accounts.User, Finances.Salary}
+# alias Ecto.Multi
+
+for [User, Salary] <- [User, Salary] do
+  Repo.delete_all(User)
+  Repo.delete_all(Salary)
+end
+
+feed_count = 200
+currencies_data = ["USD", "EUR", "JPY", "GBP", "CAD", "AUD", "CHF", "CNY", "SEK", "NZD"]
+
+names = File.read!("priv/data/names.txt")
+|> String.split("\n")
+
+Enum.map(0..feed_count, fn _ ->
+  user = User.changeset(%User{}, %{name: Enum.random(names)})
+  |> Repo.insert!()
+
+  Ecto.build_assoc(user, :salaries, %{amount: Enum.random(1000..10000), currency: Enum.random(currencies_data), active: false})
+  |> Repo.insert!()
+
+  Ecto.build_assoc(user, :salaries, %{amount: Enum.random(1000..10000), currency: Enum.random(currencies_data), active: Enum.random([true, false])})
+  |> Repo.insert!()
+
+end)
+
+IO.puts("Seeding complete!")
